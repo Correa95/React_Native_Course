eae4dc20478d41ae300badb47e130a1e4cfc6d31
Correa95 <mathewcorrea19@gmail.com>
2025-09-13 10:07:37 -0400

    created the lession2 react native

--- lession02_React_Native/app/(tabs)/index.tsx
+++ lession02_React_Native/app/(tabs)/index.tsx
@@ -1,20 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function HomeScreen() {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Home Screen 🎉</Text>
+    <View>
+      <Text>index</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 18,
-  },
-});
+export default index;
