40c89dcc33a8a1b1eb8f1ff922b33223d298479e
Correa95 <mathewcorrea19@gmail.com>
2025-09-14 12:22:14 -0400

    styling the app

--- lession02_React_Native/app/(tabs)/index.tsx
+++ lession02_React_Native/app/(tabs)/index.tsx
@@ -1,24 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
+// import React from "react";
 
-function App() {
-  return (
-    <View style={Styles.container}>
-      <Text style={Styles.text}>Coffee Shop</Text>
-    </View>
-  );
-}
+// function App() {
+//   return (
+//     <View style={Styles.container}>
+//       <Text style={Styles.text}>Coffee Shop</Text>
+//     </View>
+//   );
+// }
 
-export default App;
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+// export default App;
+// const Styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: "column",
+//   },
+//   text: {
+//     color: "white",
+//     fontSize: 42,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//   },
+// });
