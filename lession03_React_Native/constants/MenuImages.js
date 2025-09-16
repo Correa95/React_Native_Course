bb4c30273e23b0d0d32f4288f4ef0860b562058e
Correa95 <mathewcorrea19@gmail.com>
2025-09-16 10:42:40 -0400

    working with list in react native

--- lession03_React_Native/constants/MenuImages.js
+++ lession03_React_Native/constants/MenuImages.js
@@ -1,12 +1,12 @@
 import espresso from "../assets/images/menuImage/espresso.jpg";
 import Americano from "../assets/images/menuImage/americano.png";
 import latte from "../assets/images/menuImage/Latte.png";
-import Cappuccino from "../assets/images/menuImage/Cappuccino.jpg";
-import Mocha from "../assets/images/menuImage/Mocha.jpg";
+import Cappuccino from "../assets/images/menuImage/cappuccino.jpg";
+import Mocha from "../assets/images/menuImage/mocha.jpg";
 import flat_white from "../assets/images/menuImage/flat_white.jpg";
-import Cortado from "../assets/images/menuImage/Cortado.jpg";
+import Cortado from "../assets/images/menuImage/cortado.jpg";
 import coldBrew from "../assets/images/menuImage/coldBrew.jpg";
-import Affogato from "../assets/images/menuImage/Affogato.jpg";
+import Affogato from "../assets/images/menuImage/affogato.jpg";
 import irishCoffee from "../assets/images/menuImage/irishCoffee.jpg";
 import doppio from "../assets/images/menuImage/doppio.jpg";
 
