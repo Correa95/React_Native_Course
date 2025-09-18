cf30f0a367ad43384a40097b9619428e9db37556
Correa95 <mathewcorrea19@gmail.com>
2025-09-18 09:44:39 -0400

    lession 4 crud app

--- lession04_React_Native/app/_layout.jsx
+++ lession04_React_Native/app/_layout.jsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function RootLayout() {
-  return <Stack />;
+function _layout() {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
+
+export default _layout;
