2a213537d29e05f6449efacb106c78ce6bca7869
Correa95 <mathewcorrea19@gmail.com>
2025-09-19 10:07:07 -0400

    creating the render item component

--- lession04_React_Native/app/index.jsx
+++ lession04_React_Native/app/index.jsx
@@ -114,6 +114,7 @@ const styles = StyleSheet.create({
     color: "black",
   },
   todoItem: {
+    width: "100%",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -121,6 +122,5 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomColor: "gray",
     borderBottomWidth: 1,
-    width: "100%",
   },
 });
