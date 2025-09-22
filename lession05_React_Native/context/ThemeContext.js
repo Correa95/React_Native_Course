0c76cef2ceec6fa0b3abf5a8377759440b62a317
Correa95 <mathewcorrea19@gmail.com>
2025-09-22 11:28:49 -0400

    lession05 storage

--- lession05_React_Native/context/ThemeContext.js
+++ lession05_React_Native/context/ThemeContext.js
@@ -2,5 +2,7 @@ import { Children, createContext, useState } from "react";
 import { Appearance } from "react-native";
 import { colors } from "../constants/theme";
 
-const ThemeContext = createContext({})
-const ThemeProvider = ({Children})=>{const[]}
+const ThemeContext = createContext({});
+const ThemeProvider = ({ Children }) => {
+  const [colorScheme, , setColorScheme] = useState();
+};
