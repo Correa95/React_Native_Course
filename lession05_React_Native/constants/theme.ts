9c8bb8a7522c31eb0f3ce71cb0757958b15d9c99
Correa95 <mathewcorrea19@gmail.com>
2025-09-22 11:10:17 -0400

    lession05 storage

--- lession05_React_Native/constants/theme.ts
+++ lession05_React_Native/constants/theme.ts
@@ -5,16 +5,11 @@ export const Colors = {
     text: "#11181C",
     background: "#fff",
     icon: "#687076",
-    tabIconDefault: "#687076",
-    tabIconSelected: tintColorLight,
   },
   dark: {
     text: "#ECEDEE",
     background: "#151718",
-    tint: tintColorDark,
     icon: "#9BA1A6",
-    tabIconDefault: "#9BA1A6",
-    tabIconSelected: tintColorDark,
   },
 };
 
